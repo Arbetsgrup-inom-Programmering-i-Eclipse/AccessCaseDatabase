4154f632dff44101e4ec5965e16b0a95002dbe89
FlygandeFaran <erik.fura@gmail.com>
2024-03-18 14:22:50 +0100

    uppdaterat ValidateCredentials()

--- Main.cs
+++ Main.cs
@@ -24,21 +24,20 @@ namespace AccessCaseDatabase
 
             try
             {
-                string domain = "YOURDOMAIN"; //Kolla att användaren finns i Varian grupperna
+                string domain = "ltvastmanland.se"; //Kolla att användaren finns i Varian grupperna
                 PrincipalContext ctx = new PrincipalContext(ContextType.Domain, domain);
                 UserPrincipal user = UserPrincipal.FindByIdentity(ctx, Environment.UserName);
 
-                GroupPrincipal group_VarianAppUsers = GroupPrincipal.FindByIdentity(ctx, ".u.app.VarianApplicationUsers");
-                GroupPrincipal group_VarianUsers = GroupPrincipal.FindByIdentity(ctx, ".u.app.VarianUsers");
+                GroupPrincipal group_VarianUsers = GroupPrincipal.FindByIdentity(ctx, "Varian Users");
 
-                if (user != null && user.IsMemberOf(group_VarianAppUsers) && user.IsMemberOf(group_VarianUsers)) //Om användaren finns i Varian grupperna, godkänn hans/hennes credentials med lösenord
+                if (user != null && user.IsMemberOf(group_VarianUsers)) //Om användaren finns i Varian grupperna, godkänn hans/hennes credentials med lösenord
                 {
                     Check_Password.Check_Password Password_Popup = new Check_Password.Check_Password(); //Popup för att skriva in lösenordet
                     Password_Popup.ShowDialog();
 
                     if(Password_Popup.DialogResult == System.Windows.Forms.DialogResult.OK && Password_Popup.pass != null)
                     {
-                        bool isUserValid = ctx.ValidateCredentials(Environment.UserName, Password_Popup.pass);
+                        bool isUserValid = ctx.ValidateCredentials(Environment.UserName, Password_Popup.pass, ContextOptions.Negotiate);
                         Password_Popup.Dispose();
 
                         if (isUserValid)
